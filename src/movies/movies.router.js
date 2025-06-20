d5d14d977858aa3708f2e3d757d79817714377ad
Johnny Faris <jkfaris@gmail.com>
2025-06-20 15:48:43 -0700

    added method not allowed handlers to Routers.

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,9 +1,24 @@
 const router = require("express").Router();
 const controller = require("./movies.controller");
 
-router.route("/:movieId/theaters").get(controller.listTheaters);
-router.route("/:movieId/reviews").get(controller.listReviews);
-router.route("/:movieId").get(controller.read);
-router.route("/").get(controller.list);
+router
+    .route("/:movieId/theaters")
+    .get(controller.listTheaters)
+    .all((req, res, next) => next({ status: 405, error: "Method not allowed" }));
+
+router
+    .route("/:movieId/reviews")
+    .get(controller.listReviews)
+    .all((req, res, next) => next({ status: 405, error: "Method not allowed" }));
+
+router
+    .route("/:movieId")
+    .get(controller.read)
+    .all((req, res, next) => next({ status: 405, error: "Method not allowed" }));
+
+router
+    .route("/")
+    .get(controller.list)
+    .all((req, res, next) => next({ status: 405, error: "Method not allowed" }));
 
 module.exports = router;
--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const controller = require("./reviews.controller");
 
-router.route("/:reviewId").put(controller.update).delete(controller.delete);
+router
+    .route("/:reviewId")
+    .put(controller.update)
+    .delete(controller.delete)
+    .all((req, res, next) => next({ status: 405, error: "Method not allowed" }));
 
 module.exports = router;
