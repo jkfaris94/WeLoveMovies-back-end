a760263a9345a4f89d22b568d4c4b5c13ba9eb3c
Johnny Faris <jkfaris@gmail.com>
2025-06-20 15:49:37 -0700

    added router handler to theaters router

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const controller = require("./theaters.controller");
 
-router.route("/").get(controller.list);
+router
+    .route("/")
+    .get(controller.list)
+    .all((req, res, next) => next({ status: 405, error: "Method not allowed" }));
 
 module.exports = router;
